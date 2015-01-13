f90a241e3b64505c0cdfef9b7725f6f420ed37c8
Mehdi Chelli <mehdichelli.y@gmail.com>
2015-01-13 14:17:31 +0100

    creation user manager

---
+++ app/models/class.admin_users.php
@@ -0,0 +1,104 @@
+<?php
+//parent::__construct()
+class classUsers extends CoreModels {
+
+	function get_one() {
+		$query = "SELECT U.user_id, U.user_firstname, U.user_name, U.user_mail, U.user_birthday, U.user_phone, U.user_creation, S.nom, S.statut FROM " . _TABLE__USERS . " as U," . _TABLE__STATUTS . " as S WHERE U.user_id = :id AND S.type = 'user' AND S.statut = U.user_status";
+	
+		$champs = ':id';
+		$values = $this->user_id; 
+	
+		$item = $this->select_one_with_one_param($query, $values, $champs);
+	
+		return $item;
+	}
+	function get_one_array() {
+		$query = "SELECT U.user_id, U.user_firstname, U.user_name, U.user_mail, U.user_birthday, U.user_phone, U.user_creation, S.nom FROM " . _TABLE__USERS . " as U," . _TABLE__STATUTS . " as S WHERE U.user_id = :id AND S.type = 'user' AND S.statut = U.user_status";
+		
+		$cursor = $this->connexion->prepare($query);
+	
+		$cursor->bindValue(':id', $this->user_id, PDO::PARAM_INT);
+	
+		$cursor->execute();
+	
+		//$cursor->setFetchMode(PDO::FETCH_ARR);
+		$return=$cursor->fetchAll();
+		$cursor->closeCursor();
+	
+		return $return[0] ;	
+	}
+	function get_list() {
+		$orderby = 'user_id asc';
+		$this->query = "SELECT user_id, user_firstname, user_name, user_mail FROM " . _TABLE__USERS . " ORDER BY " . $orderby;
+			
+		$list = $this->select_no_param();
+		
+		return $list ;
+	}
+	
+	function create_admin() {
+		$query = "INSERT INTO " . _TABLE__ADMIN_USERS . " 
+		(mail, password, firstname, name, phone, statut) 
+		VALUES 
+		(:mail, :password, :firstname, :name, :phone, :statut)";
+		
+		$cursor = $this->connexion->prepare($this->query);
+	
+		$cursor->bindValue(':mail', $this->mail, PDO::PARAM_STR);
+		$cursor->bindValue(':password', $this->password, PDO::PARAM_STR);
+		$cursor->bindValue(':firstname', $this->firstname, PDO::PARAM_STR);
+		$cursor->bindValue(':name', $this->name, PDO::PARAM_STR);
+		$cursor->bindValue(':phone', $this->phone, PDO::PARAM_STR);
+		$cursor->bindValue(':statut', $this->statut, PDO::PARAM_INT);
+	
+		$return = $cursor->execute();
+		$cursor->closeCursor();
+		return $return ;
+	}
+	
+	function delete_user() {
+	
+			$query = "DELETE FROM " . _TABLE__USERS . " WHERE user_id = :id";
+				
+			$cursor = $this->connexion->prepare($query);
+	
+			$cursor->bindValue(':id', $this->user_id, PDO::PARAM_INT);
+			$cursor->execute();
+			$cursor->closeCursor();
+			return true ;
+	}
+	
+	function update_admin() {
+		$query = "UPDATE " . _TABLE__ADMIN_USERS . " 
+		SET mail = :mail,
+		firstname = :firstname,
+		name = :name,
+		phone = :phone,
+		statut = :statut 
+		WHERE id = :id";
+					
+		$cursor = $this->connexion->prepare($query);
+			
+		$cursor->bindValue(':mail', $this->mail, PDO::PARAM_STR);
+		$cursor->bindValue(':firstname', $this->firstname, PDO::PARAM_STR);
+		$cursor->bindValue(':name', $this->name, PDO::PARAM_STR);
+		$cursor->bindValue(':phone', $this->phone, PDO::PARAM_STR);
+		$cursor->bindValue(':statut', $this->statut, PDO::PARAM_INT);
+		$cursor->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+		$return = $cursor->execute();
+
+		$cursor->closeCursor();
+		return $return ;
+	}
+
+	function get_statuts() {
+		$query = "SELECT * FROM " . _TABLE__STATUTS . " WHERE type = 'user'";
+	
+		$list = $this->select_no_param($query);
+	
+		return $list ;
+	}
+
+}
+?>
---
+++ app/views/admin_users/display.php
@@ -0,0 +1,124 @@
+<?php include_once(_APP_PATH . 'views/includes_admin/head.inc.php'); ?>
+<?php include_once(_APP_PATH . 'views/includes_admin/header.inc.php'); ?>
+
+<div class="container" id="admin_users">
+	<div class="col-xs-12">
+	<h1 class="pull-left">Users</h1>
+	<a href="index.php?module=users&amp;action=form" class="btn btn-primary pull-right margin-top-20"><span class="glyphicon glyphicon-plus font-09em"></span> Add new</a>
+	</div>
+
+	<?php echo display_notice(); clear_notice(); ?>
+	<div class="col-xs-12 table-responsive">
+		<table class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>E-mail</th>
+					<th class="colum_action">Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				foreach($items as $item) {
+					?>
+				<tr>
+					<td><?php echo $item->user_id ; ?></td>
+					<td><?php echo $item->user_firstname ; ?></td>
+					<td><?php echo $item->user_name ; ?></td>
+					<td><?php echo $item->user_mail ; ?></td>
+					<td class="colum_action">
+						<a href="#" onclick="return false" class="tips-trigger" data-toggle="tooltip" data-placement="bottom" title="send a mail">
+							<span data-toggle="modal" data-target=".modal_mail" data-mail="<?php echo $item->user_mail ; ?>" class="glyphicon glyphicon-send modal-supp-trigger"></span>
+						</a>&nbsp;&nbsp;
+						<a href="#" onclick="return false" class="tips-trigger"  data-toggle="tooltip" data-placement="bottom" title="see details">
+							<span data-toggle="modal" data-target=".modal_fiche_user" data-id="<?php echo $item->user_id ; ?>" class="glyphicon glyphicon-eye-open modal-fiche-trigger"></span>
+						</a>&nbsp;&nbsp;
+						<a href="index.php?module=users&amp;action=form&amp;id=<?php echo $item->user_id ; ?>" class="tips-trigger" data-toggle="tooltip" data-placement="bottom" title="update">
+							<span class="glyphicon glyphicon-pencil"></span>
+						</a>&nbsp;
+						<a href="#" onclick="return false" class="tips-trigger"  data-toggle="tooltip" data-placement="bottom" title="delete">
+							<span data-toggle="modal" data-target=".bs-example-modal-sm" data-href="index.php?module=users&amp;action=delete&amp;id=<?php echo $item->user_id ; ?>" class="glyphicon glyphicon-trash modal-supp-trigger"></span>
+						</a>
+					</td>
+				</tr>
+					<?php
+				}
+				?>
+			</tbody>
+		</table>
+	</div>
+</div>
+<script type="text/javascript">
+
+	function ImageExist(url_img) 
+	{
+		var myreturn ;
+		$.ajax({
+			url: url_img, //or your url
+			success: function(){
+				$(".ajax_img").attr("src", url_img);
+			},
+			error: function(){
+				$(".ajax_img").attr("src", '<?php echo _ADMIN_PATH; ?>img/avatar.jpg');		
+			},
+		});
+	}
+	
+	function traiterFlux(flux) {
+		var obj = jQuery.parseJSON( flux );
+		var img_url = '<?php echo _ADMIN_PATH; ?>img/photo_' + obj.id + '.jpg';
+		ImageExist(img_url);
+		
+		$(".ajax_firstname").text(obj.user_firstname);
+		$(".ajax_name").text(obj.user_name);
+		$(".ajax_mail").text(obj.user_mail);
+		$(".ajax_phone").text(obj.user_phone);
+		$(".ajax_birthday").text(obj.user_birthday);
+		$(".ajax_creation").text(obj.user_creation);
+
+		//alert( obj.name );
+		//var obj = jQuery.parseJSON(flux);
+		//alert(obj.name);
+		/*$.each(flux, function(key,value) {
+			if(key == 'name') {
+			$('.ajax_name').text(value);
+			}
+			//contenu += "<li>Key : "+key+" - Value : "+value+"</li>";
+		});*/
+		//$(affichage).html(contenu);
+
+	}
+
+	
+	
+	$(document).ready(function(){
+		$('.table').DataTable();
+		$(".modal-fiche-trigger").on("click", function(){
+			var item_id = $(this).attr("data-id");
+			$.ajax({
+				// URL du traitement sur le serveur
+				url : 'index.php?module=users',
+				//Type de requête
+				type: 'post',
+				//parametres envoyés
+				data: 'action=ajax_display_user_fiche&id=' + item_id,
+				//on precise le type de flux
+				//Traitement en cas de succes
+				success: function(data) {
+					console.log("flux : " + data);
+					traiterFlux(data);
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log(textStatus + " " + errorThrown);
+					console.log("Erreur execution requete ajax");
+				}
+			});
+			
+		});
+	});
+</script>
+<?php include_once(_APP_PATH . 'views/includes_admin/modal_supp.inc.php') ?>
+<?php include_once(_APP_PATH . 'views/includes_admin/modal_mail.inc.php') ?>
+<?php include_once(_APP_PATH . 'views/includes_admin/modal_fiche_user.inc.php') ?>
