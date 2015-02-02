63e0585d14259aa81fd08946d28b35c248eaca74
webamaury <amaury.gilbon@gmail.com>
2015-02-02 18:20:59 +0100

    delete PHPCheckStyle

--- doc/PHPCheckStyle/test/issue86.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$$variable = "toto";
-
-echo $$variable;
-
-?>
--- doc/PHPCheckStyle/test/issue89.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-// Should be detected by checkProhibitedKeywordsRegExp
-$ipaddress = '192.168.1.1';
-
-// Should be detected by checkProhibitedKeywords
-$forbiddenToken = "FORBIDDEN";
